ef859857040c9ab88dd6acb5ea37de41edad832c
Dmitry Brant <dbrant@wikimedia.org>
2018-12-04 14:01:48 -0500

    Add structured descendants of Sphere.

---
+++ Hitable/CheckeredSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace BlackHoleRaytracer.Hitable
+{
+    public class CheckeredSphere : Sphere
+    {
+        private Color color1;
+        private Color color2;
+        
+        public CheckeredSphere(double centerX, double centerY, double centerZ, double radius, Color color1, Color color2)
+            : base(centerX, centerY, centerZ, radius)
+        {
+            this.color1 = color1;
+            this.color2 = color2;
+        }
+
+        protected override Color GetColor(double r, double theta, double phi)
+        {
+            var m1 = DoubleMod(phi, 1.04719); // Pi / 3
+            var m2 = DoubleMod(theta, 1.04719); // Pi / 3
+            // Pi / 6
+            return (m1 < 0.52359) ^ (m2 < 0.52359) ? color1 : color2;
+        }
+    }
+}
---
+++ Hitable/TexturedSphere.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+using BlackHoleRaytracer.Mappings;
+
+namespace BlackHoleRaytracer.Hitable
+{
+    public class TexturedSphere : Sphere
+    {
+        private SphericalMapping textureMap;
+        private int textureWidth;
+        private int[] textureBitmap;
+
+        public TexturedSphere(double centerX, double centerY, double centerZ, double radius, Bitmap texture)
+            : base(centerX, centerY, centerZ, radius)
+        {
+            textureMap = new SphericalMapping(texture.Width, texture.Height);
+            textureWidth = texture.Width;
+            textureBitmap = Util.getNativeTextureBitmap(texture);
+        }
+
+        protected override Color GetColor(double r, double theta, double phi)
+        {
+            // rotate by 180 degrees
+            phi += Math.PI;
+
+            // and retransform into cartesian coordinates
+            double tempX = 0, tempY = 0, tempZ = 0;
+            ToCartesian(r, theta, phi, ref tempX, ref tempY, ref tempZ);
+            var impactFromCenter = new Vector3((float)tempX, (float)tempY, (float)tempZ);
+
+            int xPos, yPos;
+            // hack: rejigger axes to that the texture appears right-side-up.
+            textureMap.MapCartesian(-impactFromCenter.X, impactFromCenter.Z, impactFromCenter.Y, out xPos, out yPos);
+
+            return Color.FromArgb(textureBitmap[yPos * textureWidth + xPos]);
+        }
+    }
+}
