46c746716ef6030bb1795ca7bcb6480adcd1f44d
Dmitry Brant <me@dmitrybrant.com>
2026-02-07 17:28:03 -0500

    Optimize a bit further.

--- Equation/SchwarzschildBlackHoleEquation.cs
+++ Equation/SchwarzschildBlackHoleEquation.cs
@@ -44,9 +44,7 @@ namespace BlackHoleRaytracer.Equation
             float lsq = point.X * point.X + point.Y * point.Y + point.Z * point.Z;
             float step = MathF.Sqrt(lsq) * StepSizeOver30;
 
-            point.X += velocity.X * step;
-            point.Y += velocity.Y * step;
-            point.Z += velocity.Z * step;
+            point += velocity * step;
 
             // Recompute lsq after advancing the point.
             lsq = point.X * point.X + point.Y * point.Y + point.Z * point.Z;
@@ -54,25 +52,19 @@ namespace BlackHoleRaytracer.Equation
             // x^2.5 = x * x * sqrt(x), avoiding the Pow25 lookup table.
             float sqrtLsq = MathF.Sqrt(lsq);
             float f1 = potH2 * step / (lsq * lsq * sqrtLsq);
-            velocity.X += f1 * point.X;
-            velocity.Y += f1 * point.Y;
-            velocity.Z += f1 * point.Z;
+            velocity += f1 * point;
 
             return lsq;
         }
 
         public float Function(ref Vector3 point, ref Vector3 velocity, float step)
         {
-            point.X += velocity.X * step;
-            point.Y += velocity.Y * step;
-            point.Z += velocity.Z * step;
+            point += velocity * step;
 
             float lsq = point.X * point.X + point.Y * point.Y + point.Z * point.Z;
             float sqrtLsq = MathF.Sqrt(lsq);
             float f1 = potH2 * step / (lsq * lsq * sqrtLsq);
-            velocity.X += f1 * point.X;
-            velocity.Y += f1 * point.Y;
-            velocity.Z += f1 * point.Z;
+            velocity += f1 * point;
 
             return lsq;
         }
